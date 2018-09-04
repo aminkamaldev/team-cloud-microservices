1bf30689426793966feaeab3043f51dc6e488b19
KAMAL <amin.kamal@atos.net>
2018-09-04 19:02:53 +0100

    init project

---
+++ hsup-microservice/src/main/java/com/akamal/hsupmicroservice/HsupMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.akamal.hsupmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsupMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsupMicroserviceApplication.class, args);
+	}
+}
---
+++ planning-microservice/src/main/java/com/akamal/planningmicroservice/PlanningMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.akamal.planningmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningMicroserviceApplication.class, args);
+	}
+}
